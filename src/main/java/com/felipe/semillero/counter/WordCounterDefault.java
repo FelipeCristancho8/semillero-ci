43329aac9f443befeaf327cb24643319cfe2b9ca
Felipe <ivan.cristancho@uptc.edu.co>
2022-09-14 10:48:47 -0500

    Se refactoriza la clase WordCounterDefault.java para cumplir con los requisitos de sonar

--- src/main/java/com/felipe/semillero/counter/WordCounterDefault.java
+++ src/main/java/com/felipe/semillero/counter/WordCounterDefault.java
@@ -6,18 +6,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class WordCounterDefault implements WordCounter{
 
     @Override
     public Summary process(String pathFile) throws IOException {
         Path path = Paths.get(pathFile);
-        long numberOfLines = Files.lines(path).count();
-        long numberOfWords = Files.lines(path)
-                .map(l -> l.split(" "))
-                .mapToInt(wordOfLine -> wordOfLine.length)
-                .sum();
+        long numberOfLines = getNumberOfLines(path);
+        long numberOfWords = getNumberOfWords(path);
 
         return new Summary(numberOfLines, numberOfWords);
     }
+
+    private long getNumberOfLines(Path path) throws IOException {
+        try(Stream<String> lines = Files.lines(path)) {
+            return lines.count();
+        }
+    }
+
+    private long getNumberOfWords(Path path) throws IOException {
+        try(Stream<String> lines = Files.lines(path)) {
+            return  lines
+                    .map(l -> l.split(" "))
+                    .mapToInt(wordOfLine -> wordOfLine.length)
+                    .sum();
+        }
+    }
+
+
+
 }
